0aceeb8c20a22e2f55a471181874fe99363b5829
khadija <khadijawakili652@gmail.com>
2025-06-19 18:41:56 +0430

    initialize Next.js project with MongoDB and Prisma setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+export const metadata = {
+  title: "My Etsy Clone",
+  description: "Create and delete products",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+          {children}
+      </body>
+    </html>
+  );
+}
